01354417d11ffadf5b121ddd822baa8438e924df
Ismail <ismailrzouki7@gmail.com>
2020-01-25 12:21:43 +0100

    add a conditional rendering for the loading component, we'll display an error message in case the server doesn't give any response after 6 second

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -3,10 +3,36 @@ import LoadingGif from './static/loading.gif'
 
 class Loading extends Component {
 
+    state = {
+        isLoading: true
+    }
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                isLoading: false
+            })
+        }, 6000);
+    }
+
     render() {
+        const isLoading = this.state.isLoading
         return (
             <div>
-                <img alt="loading" src={LoadingGif} />
+                {
+                    isLoading
+                    &&
+                    <img alt="loading" src={LoadingGif} />
+                }
+
+                {
+                    !isLoading
+                    &&
+                    <p>
+                        Sorry the server doesn't response, please check later!
+                    </p>
+                }
+                
             </div>
         )
     }
